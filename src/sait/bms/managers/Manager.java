4955767b15076fbbab71701ee7f0ffd472f9335c
Seungjin <Seungjin@SJ-Win10>
2021-06-05 15:06:09 -0600

    Declare a constant variable named LENGTH_OF_ISBN

--- src/sait/bms/managers/Manager.java
+++ src/sait/bms/managers/Manager.java
@@ -23,6 +23,8 @@ import sait.bms.problemdomain.Periodical;
  * @version 4 June 2021
  */
 public class Manager {
+	private final int LENGTH_OF_ISBN = 13;
+	
 	private ArrayList<Book> books = new ArrayList<>();
 	private static final String FILE_PATH = "res/books.txt";
 	Scanner in;
@@ -33,6 +35,7 @@ public class Manager {
 	 * @throws FileNotFoundException if there are any problems opening file.
 	 */
 	public Manager() throws FileNotFoundException {
+		// call a loadBooList method that loads all data from the book.txt file.
 		loadBookList();
 
 		while (true) {
@@ -46,12 +49,13 @@ public class Manager {
 
 			in = new Scanner(System.in);
 			String userInput = in.nextLine().trim();
-			
+
 			int menuNumber = 0;
-			if(isDigit(userInput)) {
+			if (isDigit(userInput)) {
 				menuNumber = Integer.parseInt(userInput);
 			}
 
+			// call each method that operates the specific feature selected by a user
 			switch (menuNumber) {
 			case 1:
 				checkout();
@@ -84,21 +88,28 @@ public class Manager {
 	 */
 	private void save() throws FileNotFoundException {
 		PrintStream output = new PrintStream(FILE_PATH);
-		for(Book book: books) {
-			if(book instanceof ChildrensBook) {
-				ChildrensBook childrensbook = (ChildrensBook)book;
-				output.println(childrensbook.getIsbn() + ";" + childrensbook.getCallNumber() + ";" + childrensbook.getAvailable() + ";" + childrensbook.getTotal() + ";" + childrensbook.getTitle() + ";" + childrensbook.getAuthors() + ";" + childrensbook.getFormat());
+		for (Book book : books) {
+			if (book instanceof ChildrensBook) {
+				ChildrensBook childrensbook = (ChildrensBook) book;
+				output.println(childrensbook.getIsbn() + ";" + childrensbook.getCallNumber() + ";"
+						+ childrensbook.getAvailable() + ";" + childrensbook.getTotal() + ";" + childrensbook.getTitle()
+						+ ";" + childrensbook.getAuthors() + ";" + childrensbook.getFormat());
 			} else if (book instanceof Cookbook) {
-				Cookbook cookbook = (Cookbook)book;
-				output.println(cookbook.getIsbn() + ";" + cookbook.getCallNumber() + ";" + cookbook.getAvailable() + ";" + cookbook.getTotal() + ";" + cookbook.getTitle() + ";" + cookbook.getPublisher() + ";" + cookbook.getDiet());
+				Cookbook cookbook = (Cookbook) book;
+				output.println(cookbook.getIsbn() + ";" + cookbook.getCallNumber() + ";" + cookbook.getAvailable() + ";"
+						+ cookbook.getTotal() + ";" + cookbook.getTitle() + ";" + cookbook.getPublisher() + ";"
+						+ cookbook.getDiet());
 			} else if (book instanceof Paperback) {
-				Paperback paperback = (Paperback)book;
-				output.println(paperback.getIsbn() + ";" + paperback.getCallNumber() + ";" + paperback.getAvailable() + ";" + paperback.getTotal() + ";" + paperback.getTitle() + ";" + paperback.getAuthors() + ";" + paperback.getYear() + ";" + paperback.getGenre());
+				Paperback paperback = (Paperback) book;
+				output.println(paperback.getIsbn() + ";" + paperback.getCallNumber() + ";" + paperback.getAvailable()
+						+ ";" + paperback.getTotal() + ";" + paperback.getTitle() + ";" + paperback.getAuthors() + ";"
+						+ paperback.getYear() + ";" + paperback.getGenre());
 			} else if (book instanceof Periodical) {
-				Periodical periodical = (Periodical)book;
-				output.println(periodical.getIsbn() + ";" + periodical.getCallNumber() + ";" + periodical.getAvailable() + ";" + periodical.getTotal() + ";" + periodical.getTitle() + ";" + periodical.getFrequency());
+				Periodical periodical = (Periodical) book;
+				output.println(periodical.getIsbn() + ";" + periodical.getCallNumber() + ";" + periodical.getAvailable()
+						+ ";" + periodical.getTotal() + ";" + periodical.getTitle() + ";" + periodical.getFrequency());
 			}
-			
+
 		}
 		output.close();
 	}
@@ -109,27 +120,26 @@ public class Manager {
 	private void produceRandomBooks() {
 		System.out.print("Enter number of books: ");
 		String userInput = in.nextLine().trim();
-		
+
 		int randomBooksNum = 0;
-		if(isDigit(userInput)) {
+		if (isDigit(userInput)) {
 			randomBooksNum = Integer.parseInt(userInput);
-		}
-		else {
+		} else {
 			System.out.println("Incorrect input. Try again.\n");
 			return;
 		}
-		
-		if(randomBooksNum > books.size()) {
+
+		if (randomBooksNum > books.size()) {
 			randomBooksNum = books.size();
 		}
-		
+
 		Collections.shuffle(books);
 		System.out.println("\nRandom books:");
 		for (int i = 0; i < randomBooksNum; i++) {
 			System.out.println(books.get(i));
 		}
 	}
-	
+
 	/**
 	 * Display the information of books by a type of book
 	 */
@@ -141,107 +151,105 @@ public class Manager {
 		System.out.println("4     Periodicals");
 		System.out.print("\nEnter type of book: ");
 		String userInput = in.nextLine().trim();
-		
+
 		int bookType = 0;
-		if(isDigit(userInput)) {
+		if (isDigit(userInput)) {
 			bookType = Integer.parseInt(userInput);
 		}
-		
+
 		switch (bookType) {
 		case 1:
 			System.out.print("Enter a format (P for Picture book, E for Early Readers, or C for Chapter book): ");
 			break;
 		case 2:
-			System.out.print("Enter a diet (D for Diabetic, V for Vegetarian, G for Gluten-free, I for International, or N for None): ");
+			System.out.print(
+					"Enter a diet (D for Diabetic, V for Vegetarian, G for Gluten-free, I for International, or N for None): ");
 			break;
 		case 3:
-			System.out.print("Enter a genre (A for Adventure, D for Drama, E for Education, C for Classic, F for Fantasy, or S for Science Fiction): ");
+			System.out.print(
+					"Enter a genre (A for Adventure, D for Drama, E for Education, C for Classic, F for Fantasy, or S for Science Fiction): ");
 			break;
 		case 4:
-			System.out.print("Enter a frequency (D for Daily, W for Weekly, M for Monthly, B for Bimonthly, or Q for Quarterly): ");
+			System.out.print(
+					"Enter a frequency (D for Daily, W for Weekly, M for Monthly, B for Bimonthly, or Q for Quarterly): ");
 			break;
 		default:
 			System.out.println("Incorrect input. Try again.\n");
 			return;
 		}
-		
+
 		userInput = in.nextLine().trim();
-		
+
 		char category = '\u0000';
-		if((userInput.length() == 1) && Character.isLetter(userInput.charAt(0)) ) {
+		if ((userInput.length() == 1) && Character.isLetter(userInput.charAt(0))) {
 			category = userInput.toUpperCase().charAt(0);
-		}
-		else {
+		} else {
 			System.out.println("Incorrect input. Try again.\n");
 			return;
 		}
-		
+
 		boolean ismatch = false;
-		
+
 		System.out.println("\nMatching books:");
-		
+
 		for (Book book : books) {
-			if(book instanceof ChildrensBook && bookType == 1) {
-				ChildrensBook childrensBook = (ChildrensBook)book;
-				if(childrensBook.getFormat() == category) {
+			if (book instanceof ChildrensBook && bookType == 1) {
+				ChildrensBook childrensBook = (ChildrensBook) book;
+				if (childrensBook.getFormat() == category) {
 					ismatch = true;
 					System.out.println(book);
 				}
-			}
-			else if(book instanceof Cookbook && bookType == 2) {
-				Cookbook cookbook = (Cookbook)book;
-				if(cookbook.getDiet() == category) {
+			} else if (book instanceof Cookbook && bookType == 2) {
+				Cookbook cookbook = (Cookbook) book;
+				if (cookbook.getDiet() == category) {
 					ismatch = true;
 					System.out.println(book);
 				}
-			}
-			else if(book instanceof Paperback && bookType == 3) {
-				Paperback paperback = (Paperback)book;
-				if(paperback.getGenre() == category) {
+			} else if (book instanceof Paperback && bookType == 3) {
+				Paperback paperback = (Paperback) book;
+				if (paperback.getGenre() == category) {
 					ismatch = true;
 					System.out.println(book);
 				}
-			}
-			else if(book instanceof Periodical && bookType == 4) {	
-				Periodical periodical = (Periodical)book;
-				if(periodical.getFrequency() == category) {
+			} else if (book instanceof Periodical && bookType == 4) {
+				Periodical periodical = (Periodical) book;
+				if (periodical.getFrequency() == category) {
 					ismatch = true;
 					System.out.println(book);
 				}
 			}
 		}
-		
-		//display for any no matching book list
-		if(!ismatch) {
+
+		// display for any no matching book list
+		if (!ismatch) {
 			System.out.println("Sorry, we can't find any matches.\n");
 		}
 	}
-
 
 	/**
 	 * Display the information of books by a title of book
 	 */
 	private void findBooksByTitle() {
 		Boolean ismatch = false;
-		
+
 		System.out.print("Enter title to search for: ");
 		String title = in.nextLine().trim();
-		
-		if(title.isBlank()) {
+
+		if (title.isBlank()) {
 			System.out.println("Sorry, incorrect input. Please enter the input.\n");
 			return;
 		}
-		
+
 		System.out.println("Matching books:");
 		for (Book book : books) {
-			if(book.getTitle().toLowerCase().contains(title.toLowerCase())) {
+			if (book.getTitle().toLowerCase().contains(title.toLowerCase())) {
 				ismatch = true;
 				System.out.println(book);
 			}
 		}
-		
-		//display for any no matching book list
-		if(!ismatch) {
+
+		// display for any no matching book list
+		if (!ismatch) {
 			System.out.println("Sorry, we can't find any matches for \"" + title + "\"\n");
 		}
 	}
@@ -251,13 +259,15 @@ public class Manager {
 	 */
 	private void checkout() {
 		System.out.print("Enter ISBN of book: ");
-		String isbn = in.nextLine();
+		String isbn = in.nextLine().trim();
 
+		// check if the isbn was input correctly
 		if (!isbnValidation(isbn)) {
 			System.out.println("Incorrect input. Try again.\n");
 			return;
 		}
 		
+		// find the book matched the isbn and display title and call number and decrement the available number
 		for (Book book : books) {
 			if (book.getIsbn() == Long.parseLong(isbn)) {
 				if (book.getAvailable() != 0) {
@@ -269,7 +279,7 @@ public class Manager {
 					System.out.println("The book \"" + book.getTitle() + "\" is not available.\n");
 					return;
 				}
-			} 
+			}
 		}
 
 		System.out.println("Sorry, we can't find any matches for \"" + isbn + "\"\n");
@@ -285,7 +295,7 @@ public class Manager {
 	private boolean isbnValidation(String isbn) {
 		boolean isIsbn = true;
 
-		if (isbn.length() != 13) {
+		if (isbn.length() != LENGTH_OF_ISBN) {
 			isIsbn = false;
 		} else {
 			isIsbn = isDigit(isbn);
@@ -293,7 +303,7 @@ public class Manager {
 		return isIsbn;
 	}
 
-	/** 
+	/**
 	 * Load book lists from books.txt file
 	 * 
 	 * @throws FileNotFoundException if there are any problems opening file.
@@ -304,8 +314,9 @@ public class Manager {
 		while (in.hasNext()) {
 			String line = in.nextLine();
 			String[] fields = line.split(";");
-			char lastDigit = fields[0].charAt(12);
-
+			char lastDigit = fields[0].charAt(LENGTH_OF_ISBN - 1);
+			
+			// add data into the ArrayList matched the correct object type
 			switch (lastDigit) {
 			case '0':
 			case '1':
@@ -314,49 +325,48 @@ public class Manager {
 				break;
 			case '2':
 			case '3':
-				books.add(new Cookbook(Long.parseLong(fields[0]), fields[1], Integer.parseInt(fields[2]), 
-				Integer.parseInt(fields[3]), fields[4],	fields[5], fields[6].charAt(0)));
+				books.add(new Cookbook(Long.parseLong(fields[0]), fields[1], Integer.parseInt(fields[2]),
+						Integer.parseInt(fields[3]), fields[4], fields[5], fields[6].charAt(0)));
 				break;
 			case '4':
 			case '5':
 			case '6':
 			case '7':
-				books.add(new Paperback(Long.parseLong(fields[0]), fields[1], Integer.parseInt(fields[2]), 
-						Integer.parseInt(fields[3]), fields[4],	fields[5], Integer.parseInt(fields[6]), fields[7].charAt(0)));
+				books.add(new Paperback(Long.parseLong(fields[0]), fields[1], Integer.parseInt(fields[2]),
+						Integer.parseInt(fields[3]), fields[4], fields[5], Integer.parseInt(fields[6]),
+						fields[7].charAt(0)));
 				break;
 			case '8':
 			case '9':
-				books.add(new Periodical(Long.parseLong(fields[0]), fields[1], Integer.parseInt(fields[2]), 
-						Integer.parseInt(fields[3]), fields[4],	fields[5].charAt(0)));
+				books.add(new Periodical(Long.parseLong(fields[0]), fields[1], Integer.parseInt(fields[2]),
+						Integer.parseInt(fields[3]), fields[4], fields[5].charAt(0)));
 				break;
 			}
 		}
 		in.close();
 	}
-	
-	
+
 	/**
 	 * Validate the input is a digit
 	 * 
 	 * @param input the user input
 	 * @return is Digit or not
 	 */
-    private boolean isDigit(String input) {
-        boolean isDigit = true;
-        
-        if(input.isBlank()) {
-            isDigit = false;
-        }
-        else {
-            for(int i=0; i < input.length(); i++){
-                if(!Character.isDigit(input.charAt(i))) {
-                    isDigit = false;
-                    break;
-                }
-            }   
-        }
-        
-        return isDigit;
-    }
-	
+	private boolean isDigit(String input) {
+		boolean isDigit = true;
+
+		if (input.isBlank()) {
+			isDigit = false;
+		} else {
+			for (int i = 0; i < input.length(); i++) {
+				if (!Character.isDigit(input.charAt(i))) {
+					isDigit = false;
+					break;
+				}
+			}
+		}
+
+		return isDigit;
+	}
+
 }
